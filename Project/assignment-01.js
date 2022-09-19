6d6066e0a7eb75d2b4975ec759271003ea2f4dec
Elishacom <113917869+Elishacom@users.noreply.github.com>
2022-09-19 17:23:02 +0100

    Add files via upload

---
+++ Project/assignment-01.js
@@ -0,0 +1,13 @@
+function increase(id){
+let oldV = parseInt(document.getElementById(id).innerText);
+ document.getElementById(id).innerText=oldV+1;
+ 
+ 
+}
+
+function decrease(d){
+let oldV = parseInt(document.getElementById(d).innerText);
+ document.getElementById(d).innerText=oldV-1;
+ 
+ 
+}
